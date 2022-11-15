d5c88a54e153f02d39988c9ac400eb94f219823e
Mandeep Gill <msgill062020@gmail.com>
2022-11-15 16:15:49 -0500

    add required fields in location migration table

--- database/migrations/2022_11_15_210338_create_locations_table.php
+++ database/migrations/2022_11_15_210338_create_locations_table.php
@@ -15,7 +15,18 @@ return new class extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
+            $table->float('latitude', 8,6);   //77.147489
+            $table ->float('longitude', 9,6); //120.211205
+            $table ->integer('user_id')->unsigned();
+
             $table->timestamps();
+
+            /*@ connect locations table's user_id with Users table's id field */
+            $table->foreign('user_id')
+                            ->references('id')
+                            ->on('users')
+                            ->onDelete('cascade');
+
         });
     }
 
