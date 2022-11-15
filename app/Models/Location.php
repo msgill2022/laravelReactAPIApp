6d5cdca5661b2415a654538d0df68839c6903c52
Mandeep Gill <msgill062020@gmail.com>
2022-11-15 16:28:51 -0500

    add one-to-many relationship with User and Location Model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Location extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'latitude',
+        'longitude'
+    ];
+    /**
+     * @return belongsTo
+     * @description get user detail of locations belongsTo
+     */
+   
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
